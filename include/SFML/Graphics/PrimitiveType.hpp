2f4682f398944e72cf4bf839d283715f8de16e7c
verrazano <hdmscratched@gmail.com>
2015-08-08 20:18:19 -0500

    Changed the PrimitiveTypes to match GL naming.

--- include/SFML/Graphics/PrimitiveType.hpp
+++ include/SFML/Graphics/PrimitiveType.hpp
@@ -40,11 +40,14 @@ enum PrimitiveType
 {
     Points,         ///< List of individual points
     Lines,          ///< List of individual lines
-    LinesStrip,     ///< List of connected lines, a point uses the previous point to form a line
+    LinesStrip = 2,     ///< Deprecated version of LineStrip
+    LineStrip = 2,     ///< List of connected lines, a point uses the previous point to form a line
     Triangles,      ///< List of individual triangles
-    TrianglesStrip, ///< List of connected triangles, a point uses the two previous points to form a triangle
-    TrianglesFan,   ///< List of connected triangles, a point uses the common center and the previous point to form a triangle
-    Quads           ///< List of individual quads (deprecated, don't work with OpenGL ES)
+    TrianglesStrip = 4,  ///< Deprecated version of TriangleStrip
+    TriangleStrip = 4, ///< List of connected triangles, a point uses the two previous points to form a triangle
+    TrianglesFan = 5,   ///< Deprecated version of TriangleFan
+    TriangleFan = 5,   ///< List of connected triangles, a point uses the common center and the previous point to form a triangle
+    Quads          ///< List of individual quads (deprecated, don't work with OpenGL ES)
 };
 
 } // namespace sf
